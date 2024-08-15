b2fe17bfbfdfa849b0721180b8ccaedf5f345a27
Gabriel Palassi <gabrielpalassi@hotmail.com>
2024-08-15 10:35:07 -0300

    Day 16

---
+++ Hashmap/contains-duplicate-II.ts
@@ -0,0 +1,11 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  let set: Set<number> = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
+    if (set.size > k) set.delete(nums[i - k]);
+  }
+
+  return false;
+}
